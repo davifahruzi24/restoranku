5df712df9f8090861200b2f8cb96f75193d33206
davifahruzi24 <davifahruzi24@gmail.com>
2025-08-26 15:37:01 +0700

    feat: add SoftDeletes and relationships to models (Category, Item, Order, OrderItem, Role, User)

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,9 +2,23 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Item extends Model
 {
-    //
+    use SoftDeletes, HasFactory;
+
+    protected $fillable = ['name', 'description', 'price', 'img', 'is_active', 'category_id', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Order extends Model
 {
-    //
+    use SoftDeletes;
+    protected $fillable = ['order_code', 'user_id', 'subtotal', 'tax', 'grandtotal', 'status', 'table _number', 'payment_method', 'note', 'created_at', 'updated_at'];
+
+    protected $dates = ['deleted_at'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
